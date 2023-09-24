f346f1e34be2980f072881c694d40f2abcd0ba45
rvictor7 <143117699+rvictor7@users.noreply.github.com>
2023-09-24 05:55:24 +0000

    twst

---
+++ src/stringTest9-23/stringTest-9-23.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main()
+{
+    std::string a = "abc";
+    std::string b = "def";
+    std::cout << a + " " + b << std::endl;
+
+
+    return 0;
+}
